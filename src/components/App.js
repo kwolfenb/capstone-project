50489f7b9e1dda7b15ddd83a0c942adcc011d151
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-03-01 14:58:54 -0800

    adds routing to app component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from './Header';
 import Body from './Body';
 
+import {   BrowserRouter as Router, Route,  Link } from 'react-router-dom'
+
 class App extends React.Component {
 
   render() {
     return (
+      <Router >
+
         <div className="App">
-          <Header />
-          <Body />
+        <Route exact path='/' component={Header}></Route>
+        <Route path='/body' component={Body}></Route>
+          
         </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer'
 import { Provider } from 'react-redux'
@@ -11,8 +10,8 @@ const store = createStore(rootReducer)
 
 ReactDOM.render(
     <Provider store={store}>
+    
         <App />
     </Provider>,
-
     document.getElementById('root'));
 
